e1f7b58ef3a31a5fdc31383e8ebf6cb7853eb298
Ngô Trương Tín <tinnt4685@ut.edu.vn>
2025-11-21 21:51:00 +0700

    Improve rental history filtering

--- CarRental/src/main/java/service/sepay/SepayWebhookData.java
+++ CarRental/src/main/java/service/sepay/SepayWebhookData.java
@@ -1,8 +1,5 @@
 package CarRental.example.service.sepay;
 
-import lombok.Data;
-
-@Data
 public class SepayWebhookData {
 
     private String tranId;
@@ -17,4 +14,91 @@ public class SepayWebhookData {
     private String transfer_type;
     private String paymentStatus;
 
+    public String getTranId() {
+        return tranId;
+    }
+
+    public void setTranId(String tranId) {
+        this.tranId = tranId;
+    }
+
+    public String getAccount_number() {
+        return account_number;
+    }
+
+    public void setAccount_number(String account_number) {
+        this.account_number = account_number;
+    }
+
+    public String getBank_name() {
+        return bank_name;
+    }
+
+    public void setBank_name(String bank_name) {
+        this.bank_name = bank_name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getSub_amount() {
+        return sub_amount;
+    }
+
+    public void setSub_amount(String sub_amount) {
+        this.sub_amount = sub_amount;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReference_number() {
+        return reference_number;
+    }
+
+    public void setReference_number(String reference_number) {
+        this.reference_number = reference_number;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getTransfer_type() {
+        return transfer_type;
+    }
+
+    public void setTransfer_type(String transfer_type) {
+        this.transfer_type = transfer_type;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
 }
